4437fdacd7849d40f2ca298110fcf38efe3696fd
massod <masood.abdulla@gmail.com>
2020-02-23 16:45:27 +0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
 
 
-//let bubble;
+let bubble;
 
 function setup(){
 
     createCanvas(400,400);
-   // bubble = new Bubble(200,200);
+    bubble = new Bubble(200,200);
 }
 
 function draw(){
 
     background(204);
-    //bubble.display();
-    ellipse(200,200,40,40);
+    bubble.display();
+    //ellipse(200,200,40,40);
 }
